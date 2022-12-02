0b501e1676062d735a7bb6dea9f747a0f83cbff0
daanielcruz <edaanxx@gmail.com>
2022-12-02 06:23:07 -0300

    remove all

--- components/Projects/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import {Content} from "./styles";
-
-const Footer = () => (
-  <Content>
-    <div>© 2020 Daniel Fernandes Cruz.</div>
-  </Content>
-);
-export default Footer;
--- translations/config.ts
+++
@@ -1,8 +0,0 @@
-export const defaultLocale = "en" as const;
-
-export const locales = ["en", "pt"] as const;
-
-export const languageNames = {
-  en: "English",
-  pt: "Português",
-};
